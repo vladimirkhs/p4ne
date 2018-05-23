c1b824cb96e1bab2ab4080ca6b021ae8fa330f51
vladimirkhs <vkhs@bk.ru>
2018-05-23 19:31:47 +0300

    complete Lab2.1

--- Lab2.1/Lab2-1.py
+++ Lab2.1/Lab2-1.py
@@ -11,14 +11,18 @@ c_num = []
 with open('C:\\Users\\resu\\p4ne\\Lab2.1\\card1.json') as data_file:
     data = json.load(data_file)
     c_num += [str(x['CreditCard']['CardNumber'])[0:8] for x in data]
+blist = []
 
 for c in c_num:
     r = requests.get(URL + c, headers={'Accept-Version': "3"})
     if 200 <= r.status_code <= 299:
         #print(r.json())
         bks = r.json()
-        #if (bks['bank']['name']) == True:
-        print(bks['bank']['name'])
+        if 'name' in bks['bank']:
+            blist.append(bks['bank']['name'])
+            #print(bks['bank']['name'])
+for ot in sorted(list(set(blist))):
+    print(ot)
 
 
 
